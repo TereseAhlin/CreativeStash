f3a3c171283ece98ef5a28e05dbfe81211a2adf6
TereseAhlin <81237638+TereseAhlin@users.noreply.github.com>
2021-03-23 15:26:16 +0100

    Add files via upload

---
+++ ö1.py
@@ -0,0 +1,24 @@
+def multiply():
+    ''' 
+    kommentar här---
+    ''' 
+    print("I detta program beräknas produkten av två tal")
+    num_1 = int(input("Ange tal 1: "))
+    num_2 = int(input("Ange tal 2: "))
+    product = num_1 * num_2
+    print("Produkten av " + str(num_1) + " och " + str(num_2) + " är: " + str(product))
+    print("--------------------------------")
+
+multiply()
+multiply()
+multiply()
+
+
+
+
+#hurMångaGångerDetSkaKöras = int(input("Ange hur många gånger det ska köras: "))
+
+#for x in range(hurMångaGångerDetSkaKöras):
+#    multiply()
+
+
